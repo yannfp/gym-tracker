10f773abc444a0fbe4303b07a7d747c37cf9e962
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-06-24 18:48:08 +0200

    feat(data.ts): constant data for the application
    Basic pages information for the navigatio bar

---
+++ frontend/src/constants/data.ts
@@ -0,0 +1,20 @@
+import { House, Dumbbell, User, Settings } from "lucide-react";
+
+export const Pages = [
+  {
+    id: "home",
+    icon: House,
+  },
+  {
+    id: "workout",
+    icon: Dumbbell,
+  },
+  {
+    id: "user",
+    icon: User,
+  },
+  {
+    id: "settings",
+    icon: Settings
+  },
+];
