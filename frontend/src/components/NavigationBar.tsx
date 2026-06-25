962ce8eef579e82dfe0cd5be2142b01d4c601c89
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-25 23:51:32 +0200

    fix(NavigationBar.tsx): use secondary background color

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -9,7 +9,7 @@ export default function NavigationBar() {
   const activeIndex = Pages.findIndex((p) => p.id === currentPage);
 
   return (
-    <div className="fixed bottom-4 left-4 right-4 flex flex-row justify-around py-3 rounded-3xl bg-primary/5">
+    <div className="fixed bottom-4 left-4 right-4 flex flex-row justify-around py-3 rounded-3xl bg-background-secondary">
 
       <div
         className='absolute top-1/2 -translate-y-1/2 w-10 h-10 rounded-full bg-accent/90 glow-accent transition-all duration-300'
