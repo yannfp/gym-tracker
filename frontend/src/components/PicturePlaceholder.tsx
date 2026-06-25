4e371aafcdf4b99bb8a8c08bdd66f10c7bd60c5c
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-25 19:30:36 +0200

    feat(PicturePlaceholder.tsx): generic picture profile placeholder

---
+++ frontend/src/components/PicturePlaceholder.tsx
@@ -0,0 +1,9 @@
+import { User } from "lucide-react";
+
+export function PicturePlaceholder() {
+  return (
+    <div className="flex items-center justify-center bg-coffee-bean-400/70 rounded-full w-15 h-15 shrink-0 text-text/50">
+      <User />
+    </div>
+  );
+}
